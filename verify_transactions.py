41d44f36eed2d9f0bd76cfefdc8662235643e601
rreusch2 <reid.reusch@gmail.com>
2024-12-12 19:56:29 -0600

    Untrack specific files and directories

--- verify_transactions.py
+++
@@ -1,7 +0,0 @@
-from models import db, Transaction
-from app import app
-
-with app.app_context():
-    transactions = Transaction.query.limit(10).all()
-    for t in transactions:
-        print(t.to_dict())
